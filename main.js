40007db9ab5d75fd7508e02e0ffe5ce1df9e7b7b
Ademuyiwa Adewuyi <ademuyiwaadewuyi@gmail.com>
2020-09-24 19:29:28 +0100

    I made my own todo app without a tutorial

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 
+const bookList = document.getElementById("book-list");
+
+//Delete Books
+bookList.addEventListener("click", function(e) {
+  if(e.target.className == "deleter") {
+    const li = e.target.parentElement;
+    bookList.removeChild(li);
+  }
+});
+
+//Add New Books
+const addBooks = document.forms[1];
+addBooks.addEventListener("submit", e => {
+  e.preventDefault();
+  const inputedText = addBooks.querySelector('input[type="text"]').value;
+  const li = document.createElement("li");
+  const bookTitle = document.createElement("span");
+  bookTitle.textContent = inputedText;
+  const deleter = document.createElement("span");
+  deleter.classList.add("deleter");
+  deleter.textContent = "delete";
+  li.appendChild(bookTitle);
+  li.appendChild(deleter);
+  bookList.appendChild(li);
+})
