043a3217d47ab17f981ce6eadceab5948c0e1af5
Ademuyiwa Adewuyi <ademuyiwaadewuyi@gmail.com>
2020-09-26 08:03:29 +0100

    added a tabbed functionality to the book app

--- main.js
+++ main.js
@@ -24,3 +24,36 @@ addBooks.addEventListener("submit", e => {
   li.appendChild(deleter);
   bookList.appendChild(li);
 })
+
+//Filter books
+const bookSearch = document.forms[0].querySelector('input[type="text"]');
+bookSearch.addEventListener("keyup", function(e) {
+  const keyword = bookSearch.value.toLowerCase();
+  const bookNames = document.querySelectorAll("li");
+  bookNames.forEach(function(title) {
+    if(title.textContent.toLowerCase().indexOf(keyword) !== -1) {
+      title.style.display = "flex";
+    }
+    else {
+      title.style.display = "none";
+    }
+  })
+})
+
+// Add a content tab at the end.
+const tabs = document.querySelector(".tabs");
+const panels = document.getElementsByClassName("panel");
+tabs.addEventListener("click", function(e) {
+  if(e.target.tagName == "LI") {
+    const li = document.querySelector(e.target.dataset.target); // To check the data attribute on the li tags.
+    Array.from(panels).forEach(panel => {
+      if(panel == li) {
+        panel.classList.add("active");
+      }
+      else {
+        panel.classList.remove("active");
+      }
+    })
+  }
+
+});
